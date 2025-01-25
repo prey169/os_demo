00b2a5010fb4488aba3aeb4cc1093626a76a881c
prey169 <francisdrossi@gmail.com>
2025-01-25 17:11:33 -0500

    Added page fault handler and then also print level 4 page table :)

--- src/interrupts.rs
+++ src/interrupts.rs
@@ -1,9 +1,9 @@
-use crate::{gdt, print, println};
+use crate::{gdt, hlt_loop, print, println};
 
 use lazy_static::lazy_static;
 use pic8259::ChainedPics;
 use spin::{self, Mutex};
-use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame};
+use x86_64::structures::idt::{InterruptDescriptorTable, InterruptStackFrame, PageFaultErrorCode};
 
 #[derive(Debug, Clone, Copy)]
 #[repr(u8)]
@@ -35,6 +35,7 @@ lazy_static! {
         }
         idt[InterruptIndex::Timer as u8].set_handler_fn(timer_interrupt_handler);
         idt[InterruptIndex::Keyboard as u8].set_handler_fn(keyboard_interrupt_handler);
+        idt.page_fault.set_handler_fn(page_fault_handler);
         idt
     };
 }
@@ -52,6 +53,18 @@ extern "x86-interrupt" fn double_fault_handler(
     _error_code: u64,
 ) -> ! {
     panic!("EXCEPTION: DOUBLE FAULT\n{:#?}", stack_frame);
+}
+
+extern "x86-interrupt" fn page_fault_handler(
+    stack_frame: InterruptStackFrame,
+    error_code: PageFaultErrorCode,
+) {
+    use x86_64::registers::control::Cr2;
+    println!("EXCEPTION: PAGE FAULT");
+    println!("Accessed Address: {:?}", Cr2::read());
+    println!("Error Code: {:?}", error_code);
+    println!("{:#?}", stack_frame);
+    hlt_loop();
 }
 
 pub const PIC_1_OFFSET: u8 = 32;
--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,13 @@ pub extern "C" fn _start() -> ! {
 
     os_demo::init();
 
+    let ptr = 0x2046a1 as *mut u8;
+
+    use x86_64::registers::control::Cr3;
+
+    let (level_4_page_table, _) = Cr3::read();
+    println!("Level 4 page table at: {level_4_page_table:?}");
+
     #[cfg(test)]
     test_main();
 
