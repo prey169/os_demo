a790cfaf6133e1f1797591e9d31a9bc716067e62
prey169 <francisdrossi@gmail.com>
2025-01-26 12:09:02 -0500

    we can now map virtual memory addresses to physical ones, except for hugepages

--- src/main.rs
+++ src/main.rs
@@ -7,35 +7,33 @@
 use bootloader::{entry_point, BootInfo};
 use core::panic::PanicInfo;
 use os_demo::println;
-use x86_64::structures::paging::PageTable;
 
 entry_point!(kernel_main);
 
 fn kernel_main(boot_info: &'static BootInfo) -> ! {
-    use os_demo::memory::active_level_4_table;
+    use os_demo::memory::translate_addr;
     use x86_64::VirtAddr;
 
     println!("Hello {}", "World");
     os_demo::init();
 
     let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-    let l4_table = unsafe { active_level_4_table(phys_mem_offset) };
 
-    for (i, entry) in l4_table.iter().enumerate() {
-        if !entry.is_unused() {
-            println!("L4 Entry {i}: {entry:?}");
+    let addresses = [
+        // vga buffer page
+        0xb8000,
+        //some code page
+        0x201008,
+        // some stack page
+        0x0100_0020_1a10,
+        // virt address mapped to physical address 0
+        boot_info.physical_memory_offset,
+    ];
 
-            let phys = entry.frame().unwrap().start_address();
-            let virt = phys.as_u64() + boot_info.physical_memory_offset;
-            let ptr = VirtAddr::new(virt).as_mut_ptr();
-            let l3_table: &PageTable = unsafe { &*ptr };
-
-            for (i, entry) in l3_table.iter().enumerate() {
-                if !entry.is_unused() {
-                    println!(" L3 Entry {i}: {entry:?}");
-                }
-            }
-        }
+    for &address in &addresses {
+        let virt = VirtAddr::new(address);
+        let phys = unsafe { translate_addr(virt, phys_mem_offset) };
+        println!("{virt:?} -> {phys:?}");
     }
 
     #[cfg(test)]
