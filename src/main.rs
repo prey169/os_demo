60d161f0546d7f44f9e75ebb3885f5ba35645401
prey169 <francisdrossi@gmail.com>
2025-01-26 22:11:19 -0500

    added heap alloc with the linked list heap allocation crate and print out the heap pointer

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@
 
 extern crate alloc;
 
-use alloc::boxed::Box;
+use alloc::{boxed::Box, rc::Rc, vec, vec::Vec};
 use bootloader::{bootinfo::MemoryRegion, entry_point, BootInfo};
 use core::panic::PanicInfo;
 use os_demo::println;
@@ -26,7 +26,27 @@ fn kernel_main(boot_info: &'static BootInfo) -> ! {
     let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
 
     allocator::init_heap(&mut mapper, &mut frame_allocator).expect("heap initialized failed");
-    let x = Box::new(41);
+    let heap_value = Box::new(41);
+    println!("heap_value at {heap_value:p}");
+
+    let mut vec = Vec::new();
+    for i in 0..500 {
+        vec.push(i);
+    }
+    println!("vec at {:p}", vec.as_slice());
+
+    let reference_counted = Rc::new(vec![1, 2, 3]);
+    let cloned_reference = reference_counted.clone();
+
+    println!(
+        "current reference count is {}",
+        Rc::strong_count(&cloned_reference)
+    );
+    core::mem::drop(reference_counted);
+    println!(
+        "reference count is {} now",
+        Rc::strong_count(&cloned_reference)
+    );
 
     #[cfg(test)]
     test_main();
